f26ce10167381a187d519916c14881480a21d40b
James <jamestomlinson8@gmail.com>
2013-10-30 21:20:25 -0700

    Added functions for place, questions, and legal moves

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,54 @@
 GRID_SIZE = 9
+SPACE = " "
+
 
 def create_grid():
     """Creates a tic tac toe grid."""
-    return [" " for cell in range(GRID_SIZE)]
+    return [SPACE for cell in range(GRID_SIZE)]
+
+
+def ask_binary_question(get_binary_response):
+    """Returns True if "y", False if "n", otherwise keeps asking."""
+    if get_binary_response() == "y":
+        return True
+
+    if get_binary_response() == "n":
+        return False
+
+    return None
+
+
+def get_binary_response():
+    """Asks the user to enter 'y' or 'n' and returns the input."""
+    return raw_input("Enter 'y' or 'n': ")
+
+
+def ask_number_question(get_number_response):
+    """Returns int if number is between 1 and 9, otherwise keeps asking."""
+    try:
+        number = int(get_number_response())
+
+    except:
+        return None
+
+    if number in range(1, GRID_SIZE + 1):
+        return number - 1
+
+
+def get_number_response():
+    """Asks the user to enter a number and returns the input."""
+    return raw_input("Enter a number: ")
+
+
+def place(grid, cell, piece):
+    """Places piece (X or O) at grid[cell]."""
+    if piece in ("X", "O"):
+        grid[cell] = piece
+
+    else:
+        raise ValueError("Piece must be 'X' or 'O.'")
+
+
+def get_legal_moves(grid):
+    """Returns a list of legal moves in the grid."""
+    return [move for move in range(GRID_SIZE) if grid[move] == SPACE]
--- unit_tests.py
+++ unit_tests.py
@@ -6,6 +6,7 @@ class CreateGridTests(unittest.TestCase):
     """Tests for the create_grid function."""
     def setUp(self):
         self.grid = tictactoe.create_grid()
+
     def test_create_grid_returns_list_of_nine_elements(self):
         grid_length = len(self.grid)
 
@@ -16,6 +17,95 @@ class CreateGridTests(unittest.TestCase):
 
         for element in self.grid:
             self.assertEqual(element, expected)
+
+
+class BinaryQuestionTests(unittest.TestCase):
+    """Tests for the ask_binary_question function."""
+    def test_ask_binary_question_with_y_returns_true(self):
+        result = tictactoe.ask_binary_question(lambda: "y")
+
+        self.assertEqual(result, True)
+
+    def test_ask_binary_question_with_n_returns_false(self):
+        result = tictactoe.ask_binary_question(lambda: "n")
+
+        self.assertEqual(result, False)
+
+    def test_ask_binary_question_with_invalid_returns_none(self):
+        result = tictactoe.ask_binary_question(lambda: "m")
+
+        self.assertIsNone(result)
+
+
+class NumberQuestionTests(unittest.TestCase):
+    """Tests for the ask_number_question function."""
+    def test_ask_number_question_with_one_returns_0(self):
+        result = tictactoe.ask_number_question(lambda: "1")
+
+        self.assertEqual(result, 0)
+
+    def test_ask_number_question_with_two_returns_2(self):
+        result = tictactoe.ask_number_question(lambda: "2")
+
+        self.assertEqual(result, 1)
+
+    def test_ask_number_question_with_invalid_number_returns_none(self):
+        result = tictactoe.ask_number_question(lambda: "0")
+
+        self.assertIsNone(result)
+
+    def test_ask_number_question_with_non_number_returns_none(self):
+        result = tictactoe.ask_number_question(lambda: "y")
+
+        self.assertIsNone(result)
+
+
+class PlacePieceTests(unittest.TestCase):
+    """Tests for the place function."""
+    def setUp(self):
+        self.grid = tictactoe.create_grid()
+
+    def test_place_with_one_and_X_sets_cell_one_to_X(self):
+        tictactoe.place(self.grid, 1, "X")
+
+        self.assertEqual(self.grid[1], "X")
+
+    def test_place_with_one_and_O_sets_cell_one_to_O(self):
+        tictactoe.place(self.grid, 1, "O")
+
+        self.assertEqual(self.grid[1], "O")
+
+    def test_place_with_invalid_piece_raises_value_error(self):
+        self.assertRaises(ValueError, tictactoe.place, self.grid, 1, "Y")
+
+
+class LegalMovesTests(unittest.TestCase):
+    """Tests for the get_legal_moves function."""
+    def setUp(self):
+        self.grid = tictactoe.create_grid()
+
+    def test_get_legal_moves_when_all_moves_legal_returns_full_list(self):
+        legal = tictactoe.get_legal_moves(self.grid)
+        legal_length = len(legal)
+
+        self.assertEqual(legal_length, 9)
+
+    def test_get_legal_moves_when_eight_moves_legal_returns_list_of_8(self):
+        self.grid[2] = "X"
+
+        legal = tictactoe.get_legal_moves(self.grid)
+        legal_length = len(legal)
+
+        self.assertEqual(legal_length, 8)
+
+    def test_get_legal_moves_when_no_moves_legal_returns_empty_list(self):
+        for i in range(9):
+            self.grid[i] = "X"
+
+        legal = tictactoe.get_legal_moves(self.grid)
+        legal_length = len(legal)
+
+        self.assertEqual(legal_length, 0)
 
 
 if __name__ == '__main__':
